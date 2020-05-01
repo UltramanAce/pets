46274a291a6cd7c8d15c9fea7fc8a186b216fd6a
UltramanAce <2496638664@qq.com>
2020-05-01 11:16:09 +0800

    新增《训练（知识）专题详情》页面

--- pages/pets/drillDetails/drillDetails.js
+++ pages/pets/drillDetails/drillDetails.js
@@ -5,7 +5,7 @@ const app = getApp()
 Page({
   data: {
     detailsData: {
-      title: '标题标题标题最多显示一行标题标题标题最多显示一行',
+      title: '主标题主标题主标标题主标题主标题主标标题主标题主标题主标标题',
       img: '/images/cat.jpg',
       details: `
       描述描述描述描述描述描述描述描述描述描述描
@@ -19,7 +19,14 @@ Page({
       述描述描述述描述描述述描述描述描述述....
       `,
       sub: '由小瓶子于2019.12.12发布'
-    }
+    },
+    images1: [
+      {
+       img: '/images/cat.jpg', // 图片路径
+       title: '难易度', // 标题
+       iconNum: 5
+     }
+   ]
   },
   onLoad: function () {
 
