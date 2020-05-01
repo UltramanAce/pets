3b187fb5f6585a42868a2ccf2f552bcda18c214c
UltramanAce <2496638664@qq.com>
2020-05-01 15:17:25 +0800

    新增《全部分类》页面

---
+++ pages/pets/classifyList/classifyList.js
@@ -0,0 +1,99 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    grids1: [
+      {
+        url: '/pages/pets/logs/logs',
+        title: '挂号',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '疫苗',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '驱虫',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '绝育',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '体检',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '寄养',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      }
+    ],
+    grids2: [
+      {
+        url: '/pages/pets/logs/logs',
+        title: '挂号',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '疫苗',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '驱虫',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '绝育',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '体检',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      },
+      {
+        url: '/pages/pets/logs/logs',
+        title: '寄养',
+        icon: 'add',
+        iconC: '#DB2C2C',
+        iconS: 30,
+      }
+    ],
+  },
+  onLoad: function () {
+
+  }
+})
