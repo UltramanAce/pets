e51b74cedf4197a5ed572ca1abeefd53f50f3efa
UltramanAce <2496638664@qq.com>
2020-05-02 19:02:56 +0800

    新增《添加宠物》页面

--- pages/pets/addPet/addPet.js
+++ pages/pets/addPet/addPet.js
@@ -1,155 +1,100 @@
 Component({
     data: {
-        showTopTips: false,
-
-        radioItems: [
-            {name: 'cell standard', value: '0', checked: true},
-            {name: 'cell standard', value: '1'}
-        ],
-        checkboxItems: [
-            {name: 'standard is dealt for u.', value: '0', checked: true},
-            {name: 'standard is dealicient for u.', value: '1'}
-        ],
-        items: [
-            {name: 'USA', value: '美国'},
-            {name: 'CHN', value: '中国', checked: 'true'},
-            {name: 'BRA', value: '巴西'},
-            {name: 'JPN', value: '日本'},
-            {name: 'ENG', value: '英国'},
-            {name: 'TUR', value: '法国'},
-        ],
-
-        date: "2016-09-01",
-        time: "12:01",
-
-        countryCodes: ["+86", "+80", "+84", "+87"],
-        countryCodeIndex: 0,
-
-        countries: ["中国", "美国", "英国"],
-        countryIndex: 0,
-
-        accounts: ["微信号", "QQ", "Email"],
-        accountIndex: 0,
-
-        isAgree: false,
+        img: "",
         formData: {
-
+            nickname: "",
+            gender: "",
+            species: "",
+            date: "",
+            sterilization: ""
         },
-        rules: [{
-            name: 'radio',
-            rules: {required: true, message: '单选列表是必选项'},
-        }, {
-            name: 'checkbox',
-            rules: {required: true, message: '多选列表是必选项'},
-        }, {
-            name: 'qq',
-            rules: {required: true, message: 'qq必填'},
-        }, {
-            name: 'mobile',
-            rules: [{required: true, message: 'mobile必填'}, {mobile: true, message: 'mobile格式不对'}],
-        }, {
-            name: 'vcode',
-            rules: {required: true, message: '验证码必填'},
-        }, {
-            name: 'idcard',
-            rules: {required: true, message: 'idcard必填'},
-        }]
+        speciesList: ["猫猫", "狗狗", "猪猪"],
+        speciesIndex: 0,
+        rules: [
+            {
+                name: 'nickname',
+                rules: {required: true, message: '宠物昵称必填'},
+            }
+        ],
+        showTopTips: false,
+        errorMsg: "",
+        error: false
     },
     methods: {
-        radioChange: function (e) {
-            console.log('radio发生change事件，携带value值为：', e.detail.value);
-    
-            var radioItems = this.data.radioItems;
-            for (var i = 0, len = radioItems.length; i < len; ++i) {
-                radioItems[i].checked = radioItems[i].value == e.detail.value;
-            }
-    
-            this.setData({
-                radioItems: radioItems,
-                [`formData.radio`]: e.detail.value
-            });
-        },
-        checkboxChange: function (e) {
-            console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-    
-            var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-            for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-                checkboxItems[i].checked = false;
-    
-                for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-                    if(checkboxItems[i].value == values[j]){
-                        checkboxItems[i].checked = true;
-                        break;
-                    }
-                }
-            }
-    
-            this.setData({
-                checkboxItems: checkboxItems,
-                [`formData.checkbox`]: e.detail.value
-            });
-        },
-        bindDateChange: function (e) {
-            this.setData({
-                date: e.detail.value,
-                [`formData.date`]: e.detail.value
-            })
-        },
-        formInputChange(e) {
-            const {field} = e.currentTarget.dataset
-            this.setData({
-                [`formData.${field}`]: e.detail.value
-            })
-        },
-        bindTimeChange: function (e) {
-            this.setData({
-                time: e.detail.value
-            })
-        },
-        bindCountryCodeChange: function(e){
-            console.log('picker country code 发生选择改变，携带值为', e.detail.value);
-    
-            this.setData({
-                countryCodeIndex: e.detail.value
-            })
-        },
-        bindCountryChange: function(e) {
-            console.log('picker country 发生选择改变，携带值为', e.detail.value);
-    
-            this.setData({
-                countryIndex: e.detail.value
-            })
-        },
-        bindAccountChange: function(e) {
-            console.log('picker account 发生选择改变，携带值为', e.detail.value);
-    
-            this.setData({
-                accountIndex: e.detail.value
-            })
-        },
-        bindAgreeChange: function (e) {
-            this.setData({
-                isAgree: !!e.detail.value.length
-            });
-        },
-        submitForm() {
+        // 提交
+        submitForm (e) {
             this.selectComponent('#form').validate((valid, errors) => {
-                console.log('valid', valid, errors)
+                console.log(e.detail.value)
+                console.log(e)
                 if (!valid) {
                     const firstError = Object.keys(errors)
                     if (firstError.length) {
                         this.setData({
-                            error: errors[firstError[0]].message
+                            errorMsg: errors[firstError[0]].message,
+                            error: true
                         })
-    
                     }
                 } else {
+                    // 组装提交数据
                     wx.showToast({
                         title: '校验通过'
                     })
+                    let obj = Object.assign({}, this.data.formData);
+                    obj.img = this.data.img[0];
+                    console.log('--------')
+                    console.log(obj)
                 }
             })
+        },
+        // 宠物昵称
+        formInputChange(e) {
+            const { field } = e.currentTarget.dataset
+            this.setData({
+                [`formData.${field}`]: e.detail.value
+            })
+        },
+        // 宠物性别
+        genderChange: function (e) {
+            console.log('宠物性别，携带value值为：', e.detail.value);
+        },
+        // 宠物品种
+        speciesChange: function(e) {
+            console.log('宠物品种，携带值为', e.detail.value);
+    
+            this.setData({
+                speciesIndex: e.detail.value
+            })
+        },
+        // 到家时间
+        bindDateChange: function (e) {
+            this.setData({
+                [`formData.date`]: e.detail.value
+            })
+        },
+        // 绝育状态
+        sterilizationChange: function (e) {
+            console.log('绝育状态，携带value值为：', e.detail.value);
+        },
+        // 调用相册或相机
+        chooseImage: function () {
+            var that = this
+            wx.chooseImage({
+              count: 9, // 最多可以选择的图片张数，默认9
+              sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+              sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+              success: function(res){
+                console.log(res)
+                that.setData({
+                    img: res.tempFilePaths
+                })
+              },
+              fail: function() {
+                // fail
+              },
+              complete: function() {
+                // complete
+              }
+            })
         }
-
     }
 });
