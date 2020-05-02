f101b6e531052b0c7bcd77ae37c081683daa0881
UltramanAce <2496638664@qq.com>
2020-05-02 22:59:36 +0800

    新增《宠物详情列表》页面

---
+++ pages/pets/petDetailsList/petDetailsList.js
@@ -0,0 +1,26 @@
+Component({
+    data: {
+        petData: {
+            img: "/images/cat.jpg",
+            name: "二哈",
+            detailsType: "随手记",
+            day: 1000,
+            like: 2
+        },
+        list: [
+            {
+                url: '../logs/logs',
+                desc: '描述描述描述描述描描述描述描述描述描描述描述述描述描述描述描述描述',
+                releaseTime: '2020-01-02'
+            },
+            {
+                url: '../logs/logs',
+                desc: '描述描述描述描述描描述描述描述描述描描述描述述描述描述描述描述描述',
+                releaseTime: '2020-01-02'
+            }
+        ]
+    },
+    methods: {
+
+    }
+});
