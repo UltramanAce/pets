dbc2e7efc32b9a87ce66c58194040670a721b6b9
UltramanAce <2496638664@qq.com>
2020-05-03 13:03:09 +0800

    新增《开启提醒（驱虫）》页面

---
+++ pages/pets/openRemindInsecticidal/openRemindInsecticidal.js
@@ -0,0 +1,25 @@
+Component({
+    data: {
+        remind: `提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述提醒建议描述`,
+        planList: ['当天提醒', '前一天提醒', '前三天提醒', '前一周提醒'],
+        planIndex: 0,
+        formData: {
+            plan: "",
+            date: "",
+        },
+        insecticidalDate: '2020-05-01',
+        nextDate: '2020-07-01'
+    },
+    methods: {
+        submitForm (e) {
+            console.log(e.detail.value)
+        },
+        bindDateChange (e) { 
+            this.setData({
+                formData: {
+                    date: e.detail.value
+                }
+            })
+        }
+    }
+});
