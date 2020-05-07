83942a2e9ff4aa0121d6390e8bb7d078b9ea6faf
UltramanAce <2496638664@qq.com>
2020-05-07 23:59:54 +0800

    新增《圈子详情》页面

---
+++ pages/pets/momentsDetails/momentsDetails.js
@@ -0,0 +1,33 @@
+Component({
+    data: {
+        momentsData: {
+            img: "/images/cat.jpg",
+            name: "二哈",
+            time: "2020.02.20 10:30",
+            question: '问题描述描述问题描述问题描述问题描述问题描述问题描述问题描述问题描述问题描述问题描述问题',
+            answer: '问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题答案问题',
+            imgList: [
+                '/images/cat.jpg',
+                '/images/cat.jpg',
+                '/images/cat.jpg'
+            ],
+            commentNum: 120,
+            heart: 12,
+        },
+        list: [
+            {
+                img: '/images/cat.jpg',
+                name: '小猫咪咪',
+                desc: '描述描述描述描述描描述描述描述描述描描述描述述描述描述描述描述描述',
+            },
+            {
+                img: '/images/cat.jpg',
+                name: '小猫咪咪',
+                desc: '描述描述描述描述描描述描述描述描述描描述描述述描述描述描述描述描述',
+            }
+        ]
+    },
+    methods: {
+
+    }
+});
